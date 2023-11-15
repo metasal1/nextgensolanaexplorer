8661c34c63d0b25c78e561aa8d0b0fcde7c2dc50
Salim Karim <imsjk8@gmail.com>
2023-11-15 16:18:06 +1100

    updated css

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -4,9 +4,7 @@ const HankoProfile = dynamic(() => import('../_lib/HankoProfile'), { ssr: false 
 
 const DashboardPage = () => {
     return (
-        <div className="flex min-h-screen flex-col items-center justify-between p-24">
-            <HankoProfile />
-        </div>
+        <HankoProfile />
     );
 };
 

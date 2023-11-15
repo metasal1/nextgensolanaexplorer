fba5f609ece200b22f19aff8df6f6c69aa074445
Salim Karim <imsjk8@gmail.com>
2023-11-15 17:57:20 +1100

    missed auth route

--- src/middleware.js
+++ src/middleware.js
@@ -19,5 +19,5 @@ export async function middleware(req) {
 }
 
 export const config = {
-    matcher: ["/:path*"],
+    matcher: ["/dashboard/:path*", "/domain/:path*", "/tokens", "/wallet/:path*"],
 };
